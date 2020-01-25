ff7c66df4a1f63b89faf113720b60e620bb38f41
piyush13891 <35322145+piyush13891@users.noreply.github.com>
2020-01-25 21:29:04 +0530

    Update IntegrationTestsCommentsValidations.java

--- src/test/java/com/IntegrationTests/IntegrationTestsCommentsValidations.java
+++ src/test/java/com/IntegrationTests/IntegrationTestsCommentsValidations.java
@@ -81,5 +81,7 @@ public class IntegrationTestsCommentsValidations {
 				}	
 			}
 		}
+		final String dir = System.getProperty("user.dir");
+        	System.out.println("current dir = " + dir);
 	}	
 }
